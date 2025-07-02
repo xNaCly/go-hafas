91fe2a57b54e679d4c77c6bf998a40e3eca79e46
xnacly <47723417+xNaCly@users.noreply.github.com>
2025-07-02 18:01:22 +0200

    autogenerated vbbraw from openapi spec

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Hafas go implementation, specifically for unternehmen.vbb.de/digitale-services/api/";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = { self, nixpkgs, }: let 
+    system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      openapiSpec = pkgs.fetchurl {
+        url = "https://vbb-demo.demo2.hafas.cloud/api/fahrinfo/latest/api-doc";
+        sha256 = "sha256-yW4BVIEKU7SEybBkNYUdmYgsQbwxJdGWKkxA6j4rYmk=";
+      };
+
+      oapi-codegen = pkgs.oapi-codegen;
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [pkgs.go oapi-codegen];
+
+        shellHook = ''
+          mkdir -p vbbraw
+          oapi-codegen \
+            -generate "client,types" \
+            -package vbbraw \
+            -o vbbraw/vbb.go \
+            ${openapiSpec}
+          go mod tidy
+        '';
+      };
+    };
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/xNaCly/go-hafas
 
 go 1.24.4
+
+require github.com/oapi-codegen/runtime v1.1.1
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+)
